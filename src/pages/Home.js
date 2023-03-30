4e85a8f05d1767ea271e693920d0740320338d35
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-03-30 19:16:19 +0200

    Create homepage file

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <div className="Home">
+            <h1>Versicherungsrechner</h1>
+            <p>Hier können sie ganz einfach ihre Hausratsversicherung berechnen</p>
+        </div>
+    )
+}
