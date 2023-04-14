1216a8bc3efda87ad2dc10f8dd715dc484b61bd1
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-04-14 14:15:54 +0200

    Create Page About

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <div className="About">
+            <h1>Über mich</h1>
+            <p>Dieses Projekt ist im Auftrag von meiner Schule KBW entstanden. Ziel dieser Seite ist den Schaden zu berechnen den die Versicherung übernimmt.</p>
+        </div>
+    )
+}
