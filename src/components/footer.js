76af2eb903441589e0ae87d34b13e41f17fdd771
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-05-11 18:04:39 +0200

    Fix Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import './style.css';
 export default function Footer(){
     return(
         <div>
-            <footer className='w-full h-16 bg-blue-100 bottom-0 left-0 flex items-center'>
+            <footer className='w-full h-16 bg-blue-100 fixed bottom-0 left-0 flex items-center'>
                 <div>
                     <p>Copyright &copy: Versicherungsrechner 2023</p>
                 </div>
