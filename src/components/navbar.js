41c7e229a8b5c61b093b7a3083dd3d44061b7f60
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-05-11 21:03:37 +0200

    Correct Navbar alignment

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,22 +3,22 @@ import './style.css';
 
 export default function Navbar(){
     return (
-        <div className="p-2 w-screen border-b-2 border-blue-600 align-center ">
-            <nav className=" max-w-auto" >
+        <div className=" flex p-2  h-[10vh] border-b-2 border-blue-600 align-center">
+            <nav className=" flex w-[80%] m-auto pt-2 ml-[500]  " >
                 <div>
-                    <ul className="flex items-center gap-[14vw]">
-                        <p className="text-[24px] font-light text-black mr-[50px]">Versicherungsrechner</p>
-                        <li className="text-[25px] font-light" >
+                    <ul className="flex items-center gap-[14vw] justify-around">
+                        <p className=" text-[18px] font-bold text-black mr-[50px]">Versicherungsrechner</p>
+                        <li className="text-[18px] font-light" >
                             <NavLink className="text-black aria-[current=page]:text-black text-blue-600" to="/">
                                 Home
                             </NavLink>
                         </li>
-                        <li className="text-[25px] font-light">
+                        <li className="text-[18px] font-light">
                             <NavLink className="text-black aria-[current=page]:text-black text-blue-600" to="/rechner" >
                                 Rechner
                             </NavLink>
                         </li>
-                        <li className="text-[25px] font-light ">
+                        <li className="text-[18px] font-light ">
                             <NavLink className="text-black aria-[current=page]:text-black text-blue-600" to="/about" >
                                 About
                             </NavLink>
