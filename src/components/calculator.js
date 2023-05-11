24f92c530f070adebffae9e04e88db73abda0663
Azhaar Mohamed <112543567+azhaarmohamed20@users.noreply.github.com>
2023-05-11 18:04:52 +0200

    Style the calculator page

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -34,15 +34,15 @@ export default function Calculator(){
     
 
     return (
-        <div className="flex flex-col justify-center items-center pt-20 ">
+        <div className="flex justify-center items-center pt-4">
             <div className="flex flex-col border-blue-200 border-[3px] rounded-[50px] p-10 ">
-                <label className="text-blue-300 text-xl">Versicherungssumme</label>
+                <label className="text-blue-300 text-[25px]">Versicherungssumme</label>
                 <input className="border-2 border-gray-500 rounded-full px-2"  type="number" name="summe" value={formValues.summe} onChange={handleOnChange}></input>
                 <br></br>
-                <label className="text-blue-300 text-xl">Versicherungswert</label>
+                <label className="text-blue-300 text-[25px]">Versicherungswert</label>
                 <input className="border-2 border-gray-500 rounded-full px-2" type="number" name="wert" value={formValues.wert} onChange={handleOnChange}></input>
                 <br></br>
-                <label className="text-blue-300 text-xl">Schaden</label>
+                <label className="text-blue-300 text-[25px]">Schaden</label>
                 <input className="border-2 border-gray-500 rounded-full px-2"  type="number" name="schaden" value={formValues.schaden} onChange={handleOnChange}></input>
                 <br></br>
                 <button className=" bg-blue-100 hover:bg-blue-200 rounded-full p-2 text-l font-bold" onClick={onclick}>Berechnen</button>
--- src/pages/Rechner.js
+++ src/pages/Rechner.js
@@ -3,8 +3,16 @@ import Calculator from "../components/calculator";
 export default function Rechner(){
     return(
         <div>
-            
+            <h1 className="flex flex-col items-center text-[40px] text-blue-300" >Schadensrechner</h1>
             <Calculator></Calculator>
+            <br></br>
+            <h3 className="flex flex-col items-center text-[30px] text-blue-400">Kurze Erklärung</h3>
+            <div className="flex flex-col justify-center items-center ">
+                <p  className="flex flex-col justify-center items-center text-[20px] text-blue-400 pb-[1rem] text-left w-[450px]">
+                    Dieser Rechner berechnet die Summe, die die Versicherung im Falle eines Schaden übernehmen würde.
+                    Mit Versicherungssumme ist die Summe gemeint versichert wurde.
+                    Mit Versicherungswert ist der akutelle Wert des Hausrats gemeint. </p>  
+            </div>
             
         </div>
     )
